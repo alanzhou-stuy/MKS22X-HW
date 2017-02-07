5a87013d66b693c1096d27ad1ad870ca30c4c5f6
Alan Zhou <azhou2@stuy.edu>
2017-02-06 20:24:35 -0500

    just set up base

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,18 @@
+public class Recursion{ 
+
+    public static String name(){
+	return "Zhou,Alan";
+    }
+
+    public static double sqrt(double n){
+
+    }
+
+    public static double helper(double value, double guess) {
+
+    }
+
+    public static boolean isCloseEnough(double firstNum, double secondNum) {
+
+    }
+}
