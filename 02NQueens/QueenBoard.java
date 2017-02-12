532f8efbbb6c4d005d7ae7fd3f0a3ad2c155dc64
Alan Zhou <azhou2@stuy.edu>
2017-02-11 22:57:35 -0500

    added code

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -10,38 +10,55 @@ public class QueenBoard{
 	solveH(0);
     }
 
-    private boolean solveH(int col,int row){
+    private boolean solveH(int col){
 	if (col == board.length) {
 	    return true;
 	}
-	for (int i = col i < board.length; i++) {
-	    for (int j = row; j < board.length; j++) {
+	for (int i = 0; i < board.length; i++) {
+	    if (board[col][i] == 0) {
+		placeQueen(col,i);
+		return solveH(col + 1);
+	    }
+	    if((i != 0) && (board[col][i] != 0)) {
+		removeQueen(col,i);
+		return solveH(col - 1);
+	    }
+	}
+	return false;
+    }
 
+    private void placeQueen(int col, int row){	
+	setRestrictions(0,col,row);
+    }
+
+    private void removeQueen(int col,int row) {
+	setRestrictions(1,col,row);
+    }
+
+    private void setRestrictions(int num,int col,int row) {
+	if (num == 0) {
+	    for (int i = 0; i == board.length; i++) {
+		if (i != row) {
+		    board[col][i] ++;
+		}
+	    }
+	    for(int i = 0; i == board.length; i++) {
+		if (i != col) {
+		    board[i][row] ++;
+		}
 	    }
 	}
     }
 
-    private void placeQueen(int col, int row) {
-	
-	setRestrictions(0);
-    }
-
-    private void removeQueen(int col, int row) {
-	
-	setRestrictions(1);
-    }
-
-    private void setRestrictions
-
     
-
+    /*
     public void countSolutions(){
     }
     
     public int getCount() {
 	
     }
-
+    */
     public String toString(){
     	return "";
     }
