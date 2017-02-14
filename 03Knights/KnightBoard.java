cf065c8a4863bcd8258bb78658609c3c57c6d2b9
Alan Zhou <azhou2@stuy.edu>
2017-02-14 06:37:34 -0500

    just copied and pasted website basic code

---
+++ 03Knights/KnightBoard.java
@@ -0,0 +1,20 @@
+public class KnightBoard {
+    private int[][]board;
+
+    public KnightBoard(int startingRows,int startingCols) {
+	board = new int[startingRows][startingCols];
+    }
+	
+    public String toString() {
+
+    } 
+
+    public void solve() {
+
+    }
+
+    private boolean solveH(int row ,int col, int num) {
+
+    }
+
+} 
