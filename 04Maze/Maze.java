f5852a2375b3df742407b12b1ac6c1d8233c4a47
Alan Zhou <azhou2@stuy.edu>
2017-03-05 10:21:43 -0500

    finished constructor

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -4,41 +4,41 @@ import java.io.*;
 public class Maze{
     private char[][]maze;
     private boolean animate;
-
-    /*Constructor loads a maze text file, and sets animate to false by default.
-      1. The file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - locations that cannot be moved onto
-      ' ' - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-
-      'S' - the location of the start(exactly 1 per file)
-
-      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-      3. When the file is not found OR there is no E or S then: print an error and exit the program.
-
-    */
-
+    private int startingX,startingY,endingX,endingY;
+    
     public Maze(String filename){
+	startingX = -1;
+	startingY = -1;
 	try {
-	    int counter,eCounter,sCounter = 0;
 	    File text = new File(filename); 
 	    Scanner inf = new Scanner(text);
-	    while(inf.hasNextLine()){
+	    int x = 0;
+	    int y = 0;
+	    while (inf.hasNextLine()) {
+		    x++;
+		    y = inf.nextLine().length();
+	    }
+	    maze = new char[x][y];
+	    int counter = 0;
+	    while(inf.hasNext()){
 		String line = inf.nextLine();
 		for (int i = 0; i < line.length(); i ++ ) {
 		    maze[counter][i] = line.charAt(i);
 		    if (line.charAt(i) == 'S') {
-			sCounter += 1;
+			startingX = counter;
+			    startingY = i;
 		    }
 		    if (line.charAt(i) == 'E')  {
-			eCounter += 1;
+			endingX = counter;
+			endingY = i;		    
 		    }
 		}
-		counter += 1;
+		    counter += 1;
 	    }
 	    animate = false;
+	    System.out.println(startingX + "," + startingY);
 	}
-	catch(FilesNotFoundException j) {
+	catch(FileNotFoundException j) {
 	    System.out.println("File not found."); 
 	    System.exit(0);
 	}	       	
@@ -63,9 +63,10 @@ public class Maze{
     }
 
     public boolean solve(){
-            int startr=-1,startc=-1;
-            maze[startr][startc] = ' ';
-            return solve(startr,startc);
+	int startr = startingX;
+	int startc = startingY;
+	maze[startr][startc] = ' ';
+	return solve(startr,startc);
     }
 
     /*
