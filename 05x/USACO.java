00b1a2eedc3af94a6cdfc0f6f6f3b82c6eeed929
Alan Zhou <azhou2@stuy.edu>
2017-03-06 18:53:21 -0500

    starting silver

--- 05x/USACO.java
+++ 05x/USACO.java
@@ -5,9 +5,16 @@ public class USACO {
     private int[][] lake;
     private int R,C,E,N,R_s,C_s,D_s;
 
-    public int bronze(String filename) {
+    private int [][] field;
+    private int N1,M1,time,R1,C1,R2,C2;
+
+    /*public int bronze(String filename) {
 	File text = new File(filename);
 	Scanner scan = new Scanner(text);
+	R = scan.nextInt();
+	C = scan.nextInt();
+	E = scan.nextInt();
+	N = scan.nextInt();
 	int counter = 1;
 	while(scan.hasNextLine()) {
 	    if (counter == 1) {
@@ -28,10 +35,53 @@ public class USACO {
 	    counter += 1;
 
 	}
+	
 
 	
     }
+*/
 
+
+    public int silver(String filename) {
+	int solution = 0;
+	try {
+	    File text = new File(filename);
+	    Scanner scan = new Scanner(text);
+	    N1 = scan.nextInt();
+	    M1 = scan.nextInt();
+	    time = scan.nextInt();
+	    field = new int[N1][M1];
+	    for (int i = 0; i < N1; i++) {
+		String x = scan.next();
+		for (int j = 0; j < M1; j++) {
+	        if (x.charAt(j) == '.') {
+		    field[i][j] = 0;
+		}
+		if (x.charAt(j) == '*') {
+		    field[i][j] = 1;
+		}
+		}	   
+	    }
+	    R1 = scan.nextInt();
+	    C1 = scan.nextInt();
+	    R2 = scan.nextInt();
+	    C2 = scan.nextInt();
+	   
+
+
+	    return solution;
+	}
+	catch(FileNotFoundException j) {
+	    System.out.println("No file found.");
+	}
+    }
+
+
+   
+    
+    public static void main(String[]args) {
+
+    }
 }
 
 
