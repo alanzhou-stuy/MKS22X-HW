a942fdbbc8d3eac1a85d29e4c0a37a67a8260d4e
Alan Zhou <azhou2@stuy.edu>
2017-03-07 15:35:32 -0500

    done

--- 05x/USACO.java
+++ 05x/USACO.java
@@ -8,14 +8,80 @@ public class USACO {
     private int [][] field;
     private int N1,M1,time,R1,C1,R2,C2;
 
-    /*public int bronze(String filename) {
-	File text = new File(filename);
-	Scanner scan = new Scanner(text);
-	R = scan.nextInt();
-	C = scan.nextInt();
-	E = scan.nextInt();
-	N = scan.nextInt();
-	int counter = 1;
+
+    public USACO(){
+    }
+
+
+    public int bronze(String filename) {
+	try {
+	    File text = new File(filename);
+	    Scanner scan = new Scanner(text);
+	    R = scan.nextInt();
+	    C = scan.nextInt();
+	    E = scan.nextInt();
+	    N = scan.nextInt();
+	    lake = new int[R][C];
+	    for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+		    lake[i][j] = scan.nextInt();
+		}	   
+	    }
+	    for (int y = 0; y < N; y++) {
+		R_s = scan.nextInt();
+		C_s = scan.nextInt();
+		D_s = scan.nextInt();
+		int large = returnLargest(R_s,C_s,lake);
+		stomp(R_s,C_s,lake,large,D_s);
+	    }	
+	    return volume(E,lake);
+	}
+	catch(FileNotFoundException e) {
+	    System.out.println("File not found.");
+	    System.exit(0);
+	}
+	return 0;
+    }
+
+    private int returnLargest(int row,int col, int[][] lake) {
+	int answer = 0;
+	for (int i = row - 1; i < row + 2; i ++ ){
+	    for (int j = col - 1; j < col + 2; j++) {
+		if (lake[i][j] >= answer) {
+		    answer = lake[i][j];
+		}
+	    }
+	}
+	return answer;
+    }
+
+    private void stomp(int row, int col, int[][] lake, int largestNum, int subtract) {
+	largestNum -= subtract;
+	for (int i = row - 1; i < row + 2; i ++ ){
+	    for (int j = col - 1; j < col + 2; j++) {
+		if (lake[i][j] > largestNum) {
+		    lake[i][j] = largestNum;
+		    }
+	    }
+	}
+    }
+
+    private int volume(int num, int[][] lake){
+	int answer = 0;
+	for (int i = 0; i < lake.length; i++) {
+	    for (int j = 0; j <lake[0].length; j++) {
+		if (num - lake[i][j] > 0) {
+		    answer += num - lake[i][j];
+		}else{
+		}
+	    }
+	}
+	return answer * 72 * 72;
+    }
+
+    
+	    
+	/*int counter = 1;
 	while(scan.hasNextLine()) {
 	    if (counter == 1) {
 		String store = scan.nextLine();
@@ -34,15 +100,9 @@ public class USACO {
 	    }
 	    counter += 1;
 
-	}
-	
+	    }*/
 
-	
-    }
-*/
-
-
-    public int silver(String filename) {
+    /*public int silver(String filename) {
 	int solution = 0;
 	try {
 	    File text = new File(filename);
@@ -74,13 +134,14 @@ public class USACO {
 	catch(FileNotFoundException j) {
 	    System.out.println("No file found.");
 	}
-    }
+	}*/
 
 
    
     
     public static void main(String[]args) {
-
+	USACO x = new USACO(); //does not have to do anything. 
+	System.out.println(x.bronze("test1.java"));
     }
 }
 
