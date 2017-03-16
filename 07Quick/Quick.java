f7a778ac3caa33e580c337b0e478e04c8314c8fc
Alan Zhou <azhou2@stuy.edu>
2017-03-16 00:10:59 -0400

    doesnt work yet

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -4,59 +4,68 @@ import java.io.*;
 public class Quick {
 
     public static int quickselect(int []data, int k){
-	int answer = 0;
-	int temp = part(data,0,data.length);	
-	//	if (data[k] == k) {
-	//    return answer;
-	//}
-	for (int i = 0; i == k - 1 ; i++) {
-	    temp = part(data,0,temp);
-	    if (data[k] == k) {
-		return answer;
-	    }
+	return helper (data,k,0,data.length - 1 );
+    }
+
+    public static int helper(int[] data,int k, int start, int end) {
+	if ( start == end ){
+	    return data[start];
 	}
-	return -1;
+	int temp = part(data,start,end);
+	if (temp == k) {
+	    return data[temp];
+	}
+	else if (k < temp) {
+	    return helper(data,k,start,temp - 1);
+	}
+	else {
+	    return helper(data,k,temp + 1, end);
+	}
+	
     }
 
     public static int part ( int [] data, int start, int end){
-	//int randomPivot = data[(int)(Math.random() * data.length)];
-	int randomPivot = 15;
-	//int[] temp = new int[data.length];
-	int counter = 1;
+	int randomPivot = data[(int)(start + Math.random() * (end - start + 1) )];
+	//int randomPivot = 0;
 	int counter1 = 0;
-	for (int i = start; i < end; i ++) {
-	    if (data[i] == randomPivot) {		
+	int counter = 0 ;
+	for (int i = start; i <= end ; i ++) {
+	    if (data[i] < randomPivot) {
+		//System.out.println(i + ":" + counter1);
+		data[counter1] = data[i];
+		counter1 += 1;
 	    }
-	    else if (data[i] > randomPivot) {
-		int temp = data[data.length - counter];
-		data[data.length - counter] = data[i];
+	    if (data[i] > randomPivot) {
+		//System.out.println("xd");
+		int temp = data[data.length  - 1 - counter];
+		data[data.length - 1 - counter] = data[i];
 		counter += 1;
 		data[i] = temp;
 		i --;
 	    }
-	    else if (data[i] < randomPivot) {
-		data[counter1] = data[i];
-		counter1 += 1;
+	    if (counter  + counter1 == end - start) {
+		data[i] = randomPivot; 
+		System.out.println("Pivot: " + randomPivot);
+		return i;
 	    }
-	    if (counter1 + counter == end) {
-		data[i] = randomPivot;
-	    }
+	   
 	}
+	System.out.println("Pivot: " + randomPivot);
 	return -1;
     }
     public static void main(String[] args) {
 	int[]ary = { 2, 10, 15, 23, 0,  5};
-	System.out.println(part(ary,0,5));
+	System.out.println(part(ary,1,5) + "\n");
 	for (int i = 0; i < 6; i++) {
 	    System.out.println(ary[i]);
 	}
-
-	/*System.out.println(quickselect( ary , 0 ));
+	/*
+	System.out.println(quickselect( ary , 0 ));
 	System.out.println(quickselect( ary , 1 ));  
 	System.out.println(quickselect( ary , 2 ));  
 	System.out.println(quickselect( ary , 3 ));  
 	System.out.println(quickselect( ary , 4 ));  
-	System.out.println(quickselect( ary , 5 ));  
+	System.out.println(quickselect( ary , 5 ));  	
 	*/
 	}
 }
