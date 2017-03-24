e24a17b62e3f3effcc52490cec1288652a07a761
Alan Zhou <azhou2@stuy.edu>
2017-03-23 21:58:50 -0400

    started

---
+++ 08Merge/Merge.java
@@ -0,0 +1,27 @@
+public class Merge {
+
+    public static void mergeSort(int[]ary) {
+	if (true){
+	}
+	else {
+	    int[]left = copy of the left side;
+	    int[]right = copy of the right side;
+	    mergesort(left);
+	    mergesort(right);
+	    mergeTheTwoHalvesIntoTheOriginalArray;
+	}
+    }
+
+    /*
+    Postcondition:
+    destination contains all of the elements of a and b, and is sorted. 
+    Preconditions:
+    a is sorted, b is sorted
+    destination.length == a.length + b.length
+    */
+
+    public static void merge (int[]a,int[]b,int[]destination) {
+
+    }
+
+}
