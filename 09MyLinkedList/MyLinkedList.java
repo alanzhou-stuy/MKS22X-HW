b17fd69d0b75024a0c55a8a71b96d4a4726c1b5a
Alan Zhou <azhou2@stuy.edu>
2017-03-28 00:26:24 -0400

    started

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,25 @@
+public class MyLinkedList {
+    LNode start;
+    LNode end;
+    int size;
+
+    public String toString() {
+
+
+    }
+
+    public void add() {
+	
+    }
+    
+    private class LNode {
+	public int value;
+	private Lnode next, previous;
+	
+	public LNode (int x) {
+	    value = x;
+	}
+    }
+
+
+}
