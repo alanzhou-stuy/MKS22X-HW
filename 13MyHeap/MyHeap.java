15685bfec475f6bad92fe8d3c5caddb5ba7622d8
Alan Zhou <azhou2@stuy.edu>
2017-05-04 15:31:40 -0400

    almsot

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,4 +1,6 @@
-public class MyHeap() {
+import java.util.*;
+
+public class MyHeap {
     String[] array;
     int constant;
     int size;
@@ -18,27 +20,89 @@ public class MyHeap() {
 	array = new String[17];
 	size = 0;
     }
+    
+    private void resize() {
+	String[] temp = new String[size * 2 + 1];
+	for (int i = 1; i < array.length; i++) {
+	    temp[i] = array[i];
+	}
+	array = temp;
+    }
 
     public void add (String s) {
+	if (size == array.length) {
+	    resize();
+	}
 	array[size + 1] = s;
 	size ++;
-	pushUp();
+	if (size > 1)  {
+	    pushUp();
+	}
     }
 
     public String remove() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	}
+	String temp = array[1];
+	array[1] = array[size];
+	array[size] = null;
+	pushDown();
 	size --;
-	array[1];
+	return temp;
     }
 
     public String peek() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	}
 	return array[1];
     }
 
     private void pushUp() {
-
+	String x = array[size];
+	for (int i = size; i > 1; i =  i / 2){
+	    if (array[i].compareTo(array[i/2]) * constant >= 0) {
+		    array[i] = array[i/2];
+		    array[i/2] = x;
+		}
+	    else {
+		return;
+	    } 
+	}
     }
 
     private void pushDown() {
-
+	for (int i = 1; i < size; i = i *2) {
+	    if (array[i].compareTo(array[i + 1]) * constant >= 0) {
+		array[i / 2] = array[i];
+	    }
+	    else {
+		array[i / 2] = array[i + 1];
+	    }
+	}
     }
+    
+    public String toString() {
+	String temp = "";
+	for (int i = 1; i <= size; i ++) {
+	    temp += " " + array[i];
+	}
+	return temp;
+    }
+
+    public static void main(String[] args) {
+	MyHeap test = new MyHeap(true);
+	test.add("A ");
+	test.add("B ");
+	test.add("C ");
+	test.add("D ");
+	test.add("E ");
+	test.add("F ");
+	test.add("G ");
+	test.add("H ");
+	test.remove();
+	System.out.println(test);
+    }
+    
 }
