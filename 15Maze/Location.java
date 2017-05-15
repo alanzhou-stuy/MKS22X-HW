2f295a0ee008a2578506eb46a30bf904257d6a88
Alan Zhou <azhou2@stuy.edu>
2017-05-15 15:29:49 -0400

    started

---
+++ 15Maze/Location.java
@@ -0,0 +1,24 @@
+public class Location implements Comparable<Location>{
+    private int row,col;
+    private int distToGoal;
+    private int distToStart;
+    private Location previous;
+    private boolean aStar;
+    
+    public Location(int r, int c, Location previous, int distToStart, int distToGoal, boolean aStar) {
+	
+    }
+
+    public int getDistToStart() {
+
+    }
+
+    public int getDistToGoal() {
+
+    }
+    
+    public Location compareTo(Location other) {
+
+    }
+
+}
