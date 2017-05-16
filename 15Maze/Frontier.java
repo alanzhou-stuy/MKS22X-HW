966dd3465f43ebb4b143e2410795f7630b344489
Alan Zhou <azhou2@stuy.edu>
2017-05-16 15:28:26 -0400

    lab prd

--- 15Maze/Frontier.java
+++ 15Maze/Frontier.java
@@ -1,9 +1,14 @@
 public interface Frontier{
+    private int size;
 
     public void add(Location l) {
     }
     
     public Location next() {
+    }
+
+    public int getSize() {
+	return size;
     }
 
 
--- 15Maze/Location.java
+++ 15Maze/Location.java
@@ -5,20 +5,57 @@ public class Location implements Comparable<Location>{
     private Location previous;
     private boolean aStar;
     
-    public Location(int r, int c, Location previous, int distToStart, int distToGoal, boolean aStar) {
-	
+    public Location(int r, int c, Location previous1, int distToStart1, int distToGoal1, boolean aStar1) {
+	row = r;
+	col = c;
+	previous = previous1;
+	distToStart = distToStart1;
+	distToGoal = distToGoal1;
+	aStar = aStar1;
     }
 
     public int getDistToStart() {
-
+	return distToStart;
     }
 
     public int getDistToGoal() {
+	return distToGoal;
+    }
 
+    public int getRow() {
+	return row;
+    }
+
+    public int getCol() {
+	return col;
+    }
+
+    public boolean getAStart() {
+	return aStar;
     }
     
-    public Location compareTo(Location other) {
-
+    public int compareTo(Location other) {
+	if (aStar) {
+	    if (distToStart + distToGoal == other.getDistToStart() + getDistToGoal()) {
+		return 0;
+	    }
+	    else if (distToStart + distToGoal > other.getDistToStart() + getDistToGoal()) {
+		return 1;
+	    }
+	    else  {
+		return -1;
+	    }
+	}
+	else {
+	    if (distToGoal == other.getDistToGoal()) {
+		return 0;
+	    }
+	    else if (distToGoal > other.getDistToGoal()) {
+		return 1;
+	    }
+	    else {
+		return -1;
+	    }
+	}
     }
-
 }
