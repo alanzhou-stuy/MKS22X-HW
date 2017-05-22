8a18a0dc71c03d95e3d6810be7cfa189a125d412
Alan Zhou <azhou2@stuy.edu>
2017-05-21 23:20:29 -0400

    .

--- 15Maze/MazeSolver.java
+++ 15Maze/MazeSolver.java
@@ -2,13 +2,20 @@ import java.util.*;
 
 public class MazeSolver {
     private Maze board;
+    private boolean animate;
+    private aStar;
+    private Location e;
+    private int row, col;
     
     public MazeSolver(String filename) {
-
+	this(filename);
+	aStar = false;
     }
 
     public MazeSolver(String filename, boolean animate) {
-       
+	this(filename);
+	this(animate);
+	astar = false;
 
     }
 
@@ -17,7 +24,25 @@ public class MazeSolver {
     }
 
     public void solve(int a) {
-
+	Frontier f;
+	row = board.getRow();
+	col = board.getCol();
+	if(x == 0){
+	    f = new FrontierStack();
+	}
+	else if(x == 1){
+	    f = new FrontierQueue();
+	}
+	else if(x == 2){
+	    f = new FrontierPriorityQueue();
+	}
+	else if(x == 3){
+	    f = new FrontierPriorityQueue();
+	    astar = true;
+	}
+	e = board.getEnd();
+	f.add(board.getStart());
+	go();
     }
 
     public String toString() {
